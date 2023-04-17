6e67bf1b319d5d0952e8a7355ce5081cb6bff3e7
walacefex <walacefex01@gmail.com>
2023-04-17 20:20:47 -0300

    Criando o teste para validar a função de deposito e transferência, usando jest.fn()

---
+++ src/utils/utils.test.js
@@ -0,0 +1,21 @@
+import { calculaNovoSaldo } from './index';
+
+describe('Quando eu realizo uma transação', () =>{
+  test('Que é um deposito, o saldo deve aumente', () =>{
+    const transacao = {
+      transacao: 'Depósito',
+      valor: 50,
+    };
+
+    const novoSaldo = calculaNovoSaldo(transacao, 100);
+    expect(novoSaldo).toBe(150);
+  })
+})
+test('Deve retornar o valor do saldo atualizado com o rendimento', () => {
+  const calculaRendimento = jest.fn((saldo) => saldo + saldo * 0.005);
+  const saldo = 100;
+  const novoSaldo = calculaRendimento(saldo);
+  expect(novoSaldo).toBe(100.5)
+  expect(calculaRendimento).toBeCalled();
+  expect(calculaRendimento).toHaveBeenCalledWith(saldo);
+})
